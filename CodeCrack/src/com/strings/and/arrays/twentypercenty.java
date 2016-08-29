d1efe5c91828d27b28b1412811a176de1aa93dee
Anup Ahire <anupahire@MacBook-Pro.local>
2016-08-28 20:10:37 -0700

    First Commit

---
+++ CodeCrack/src/com/strings/and/arrays/twentypercenty.java
@@ -0,0 +1,37 @@
+package com.strings.and.arrays;
+
+public class twentypercenty {
+	public static void main (String [] args) {
+		
+		String str ="the dog is";
+		 char [] arr = str.toCharArray();
+		 int spaceCount=0;
+		 for (int i=0;i<arr.length;i++) {
+			 if(arr[i]==' ') spaceCount++;
+		 }
+		 
+		 int newLength=arr.length+(spaceCount*2);
+		 char [] newArr = new char [newLength];
+		// newArr[newLength]='\0';
+		 
+		 for (int j=arr.length-1;j>=0;j--) {
+			 if (arr[j]==' ') {
+				 newArr[newLength-1]='0';
+				 newArr[newLength-2]='2';
+				 newArr[newLength-3]='%';
+				 newLength=newLength-3;
+			 }else {
+				 newArr[newLength-1]=arr[j];
+				 newLength--;
+			 }
+		 }
+		 
+		 System.out.println(newArr);
+		 
+		 
+		 
+		 
+		
+	}
+
+}
