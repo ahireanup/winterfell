8cf01304e7f73f27bef3868c47986aa039dd78a6
Anup <ahireanup@gmail.com>
2017-02-19 22:10:16 -0800

    valid parenthesis

---
+++ CodeCrack/src/com/strings/and/arrays/ValidParenth.java
@@ -0,0 +1,49 @@
+package com.strings.and.arrays;
+
+import java.util.HashMap;
+
+/*
+ * Given a string containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+ */
+public class ValidParenth {
+
+	public static boolean isValid(String str) {
+
+		if (str.length() <= 1) {
+			return false;
+		} else if (str.length() % 2 != 0) {
+			return false;
+		}
+
+		HashMap<Character, Character> charMap = new HashMap<Character, Character>();
+		charMap.put('[', ']');
+		charMap.put('(', ')');
+		charMap.put('{', '}');
+
+		int i = 0;
+		int j = str.length() - 1;
+		char[] arr = str.toCharArray();
+		while (i <= j) {
+			if (charMap.containsKey(arr[i])) {
+				if (charMap.get(arr[i]) != arr[j]) {
+					return false;
+				}
+			} else {
+				return false;
+			}
+			i++;
+			j--;
+
+		}
+
+		return true;
+	}
+
+	public static void main(String[] args) {
+
+		boolean result = isValid("[[[[]]]");
+		System.out.println(result);
+		;
+	}
+
+}
