2d4cc038bca44bc23c89c8ac8ac9fb78242a3474
Anup <ahireanup@gmail.com>
2017-02-21 22:41:23 -0800

    push

---
+++ CodeCrack/src/com/strings/and/arrays/ReverseString.java
@@ -0,0 +1,31 @@
+package com.strings.and.arrays;
+
+public class ReverseString {
+
+	public static String reverseString(String s) {
+
+		if (s.length() <= 1) {
+
+			return s;
+
+		}
+		char[] arr = s.toCharArray();
+
+		StringBuffer buffer = new StringBuffer();
+
+		for (int i = arr.length - 1; i >= 0; i--) {
+			buffer.append(arr[i]);
+		}
+
+		return buffer.toString();
+
+	}
+
+	public static void main(String[] args) {
+
+		System.out.println(reverseString("ABC"));
+		;
+
+	}
+
+}
